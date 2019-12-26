82ae8114e3a2ff7b9bc739eb703893c5a3e39056
DELL <annadurai.mca10@gmail.com>
2019-12-26 09:55:21 +0530

    @ManyToMany

---
+++ Hibernate_mtmannotation2/src/main/java/com/StoreData.java
@@ -0,0 +1,60 @@
+package com;  
+import java.util.ArrayList;  
+import org.hibernate.*;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+   
+public class StoreData {  
+public static void main(String[] args) {  
+   
+	StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+    Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();  
+    SessionFactory factory=meta.getSessionFactoryBuilder().build();  
+    Session session=factory.openSession();  
+    Transaction t=session.beginTransaction();    
+        
+	Answer an1=new Answer();
+	an1.setAnswername("Java is a programming language");
+	an1.setPostedBy("Ravi Malik");
+	
+	Answer an2=new Answer();
+	an2.setAnswername("Java is a platform");
+	an2.setPostedBy("Sudhir Kumar");
+   
+	Question q1=new Question();
+	q1.setQname("What is Java?");
+	ArrayList<Answer> l1=new ArrayList<Answer>();
+	l1.add(an1);
+	l1.add(an2);
+	q1.setAnswers(l1);
+	
+	
+	  Answer ans3=new Answer();  
+	    ans3.setAnswername("Servlet is an Interface");  
+	    ans3.setPostedBy("Jai Kumar");  
+	      
+	    Answer ans4=new Answer();  
+	    ans4.setAnswername("Servlet is an API");  
+	    ans4.setPostedBy("Arun");  
+	
+	Question q2=new Question();
+	q2.setQname("What is Servlet?");
+	ArrayList<Answer> l2=new ArrayList<Answer>();
+	l2.add(ans3);
+	l2.add(ans4);
+	q2.setAnswers(l2);
+
+
+      
+    session.persist(q1);  
+    session.persist(q2);  
+      
+    t.commit();  
+    session.close();  
+    System.out.println("success");  
+    
+     
+}  
+}  
---
+++ Hibernate_mtmannotation2/src/main/java/com/mtmannotation/App.java
@@ -0,0 +1,13 @@
+package com.mtmannotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
